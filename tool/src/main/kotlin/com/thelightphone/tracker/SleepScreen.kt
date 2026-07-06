9f6cf05d062d85172c6831985b9d5889e4369fae
tyshi00 <tyshi00@gmail.com>
2026-07-06 13:32:00 -0400

    Fix compile error: remove unnecessary weight import in SleepScreen

--- tool/src/main/kotlin/com/thelightphone/tracker/SleepScreen.kt
+++ tool/src/main/kotlin/com/thelightphone/tracker/SleepScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
